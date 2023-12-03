a628aa6df5088fd5be3364d017d21b8f1a394eff
Marek Macho <majky.macho@gmail.com>
2023-12-03 22:14:46 +0100

    refactor: navbar to use ul

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,14 +4,20 @@ import Logo from '../images/TEDxTrencin_logo.png';
 export const Navbar = () => (
   <nav className="flex flex-wrap items-center justify-between px-10 border-2">
     <Image src={Logo} alt="logo" width="200" />
-    <div className="flex">
-      <Link className="mr-6 flex flex-shrink-0 items-center" href="/">
-        <span className="text-xl font-semibold tracking-tight">Program</span>
-      </Link>
-      <Link className="mr-6 flex flex-shrink-0 items-center" href="/speakers">
-        <span className="text-xl font-semibold tracking-tight">Rečníci</span>
-      </Link>
-      <button className="bg-red p-4 rounded">Prihlásenie</button>
-    </div>
+    <ul className="flex">
+      <li>
+        <Link className="mr-6 flex flex-shrink-0 items-center" href="/">
+          <span className="text-xl font-semibold tracking-tight">Program</span>
+        </Link>
+      </li>
+      <li>
+        <Link className="mr-6 flex flex-shrink-0 items-center" href="/speakers">
+          <span className="text-xl font-semibold tracking-tight">Rečníci</span>
+        </Link>
+      </li>
+      <li>
+        <button className="bg-red p-4 rounded">Prihlásenie</button>
+      </li>
+    </ul>
   </nav>
 );
