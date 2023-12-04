582f03641957779c4da9599bbd48fce08069c339
Marek Macho <majky.macho@gmail.com>
2023-12-04 19:03:50 +0100

    fix: tag to properly use ColorType

--- src/components/Tags.tsx
+++ src/components/Tags.tsx
@@ -1,12 +1,24 @@
-import Chip from '@mui/material/Chip';
+import Chip, { ChipPropsColorOverrides } from '@mui/material/Chip';
 import Stack from '@mui/material/Stack';
 import { Tag } from '@prisma/client';
+import { OverridableStringUnion } from '@mui/types';
+
+type ColorType = OverridableStringUnion<
+  | 'default'
+  | 'primary'
+  | 'secondary'
+  | 'error'
+  | 'info'
+  | 'success'
+  | 'warning',
+  ChipPropsColorOverrides
+>;
 
 export const Tags = ({ tags }: { tags: Tag[] }) => {
   return (
     <Stack direction="row" spacing={1}>
       {tags.map((t) => (
-        <Chip key={t.id} label={t.title} color={t.color} />
+        <Chip key={t.id} label={t.title} color={t.color as ColorType} />
       ))}
     </Stack>
   );
