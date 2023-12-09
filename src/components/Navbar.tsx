bb7979df671cb86e80218d847f315ad0b1bf4e86
Magdaléna <492966@mail.muni.cz>
2023-12-09 19:36:21 +0100

    fix navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,16 +3,13 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import Logo from '../images/TEDxTrencin_logo.png';
-import { signIn } from 'next-auth/react';
-
-const signInUsingDiscord = () => {
-  signIn('discord')
-};
 
 export const Navbar = () => (
-  <nav className="flex flex-wrap items-center justify-between px-10 border-2">
-    <Image src={Logo} alt="logo" width="200" />
-    <ul className="flex">
+  <nav className="flex flex-wrap items-center justify-between px-10">
+    <Link href="/">
+      <Image src={Logo} alt="logo" width="200" />
+    </Link>
+    <ul className="flex text-base">
       <li>
         <Link className="mr-6 flex flex-shrink-0 items-center" href="/">
           <span className="text-xl font-semibold tracking-tight">Program</span>
@@ -27,9 +24,6 @@ export const Navbar = () => (
         <Link className="mr-6 flex flex-shrink-0 items-center" href="/tags">
           <span className="text-xl font-semibold tracking-tight">Tagy</span>
         </Link>
-      </li>
-      <li>
-        <button className="bg-red p-4 rounded" onClick={signInUsingDiscord}>Prihlásenie</button>
       </li>
     </ul>
   </nav>
