a525022fd3b9cd0e3d7b33f95fedbd2545f39240
Magdaléna <492966@mail.muni.cz>
2023-12-11 22:50:50 +0100

    fix slido title

--- src/app/speakers/page.tsx
+++ src/app/speakers/page.tsx
@@ -3,6 +3,24 @@ import ListOfDetailInformationPage from '@/components/ListOfDetailInformation';
 
 export const metadata: Metadata = {
   title: 'Rečníci',
+  description:
+    'TEDxTrenčín poskytuje platformu pre zaujimavých a inšpiratívnych ľudí, ktorých myšlienky, nápady a činy sú hodné zdieľania.',
+  metadataBase: new URL('https://www.tedxtrencin.sk/images/facebook.jpg'),
+  twitter: {
+    card: 'summary_large_image',
+    title: 'TEDxTrenčín',
+    description:
+      'TEDxTrenčín poskytuje platformu pre zaujimavých a inšpiratívnych ľudí, ktorých myšlienky, nápady a činy sú hodné zdieľania.',
+    images: [
+      {
+        url: 'https://www.tedxtrencin.sk/images/facebook.jpg',
+        type: 'image/jpg',
+        width: 1200,
+        height: 630,
+        alt: 'tedx',
+      },
+    ],
+  },
 };
 
 const DetailInformationPage = () => {
