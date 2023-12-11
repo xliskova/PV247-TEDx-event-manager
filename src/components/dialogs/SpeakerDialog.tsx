379d4bd5150b76e2e2a2ba16a8ceceafe5b42e73
Marek Seďa <marek.seda@gmail.com>
2023-12-11 20:17:55 +0100

    format speaker dialog

--- src/components/dialogs/SpeakerDialog.tsx
+++ src/components/dialogs/SpeakerDialog.tsx
@@ -1,66 +1,79 @@
-import { useState } from "react";
-import { BasicDialog } from "./BasicDialog";
-import { Speaker } from "@/model/Speaker";
-
-
+import { useState } from 'react';
+import { BasicDialog } from './BasicDialog';
+import { Speaker } from '@/model/Speaker';
 
 type SpeakerDialogProps = {
-    speaker: Speaker | undefined,
-    onSubmit: (speaker: Speaker, image: File) => void,
-    close: () => void,
-    isOpen: boolean,
+  speaker: Speaker | undefined;
+  onSubmit: (speaker: Speaker, image: File) => void;
+  close: () => void;
+  isOpen: boolean;
 };
 
-export const SpeakerDialog = ({ speaker, onSubmit, close, isOpen }: SpeakerDialogProps) => {
-    const [image, setImage] = useState<File | undefined>(undefined);
+export const SpeakerDialog = ({
+  speaker,
+  onSubmit,
+  close,
+  isOpen,
+}: SpeakerDialogProps) => {
+  const [image, setImage] = useState<File | undefined>(undefined);
 
-    const handleFileChange = (event: any) => {
-        setImage(event.target.files[0])
-    };
-    return (<BasicDialog 
-    value={speaker} 
-    getFields={(speaker) => [
+  const handleFileChange = (event: any) => {
+    setImage(event.target.files[0]);
+  };
+  return (
+    <BasicDialog
+      value={speaker}
+      getFields={(speaker) => [
         {
-            key: 'name',
-            title: 'Jméno',
-            input: (form) => <input
-                type="text"
-                id="name"
-                {...form.register('value.name', { required: 'Name is required' })}
-                className="w-full p-2 border rounded"
+          key: 'name',
+          title: 'Jméno',
+          input: (form) => (
+            <input
+              type="text"
+              id="name"
+              {...form.register('value.name', { required: 'Name is required' })}
+              className="w-full p-2 border rounded"
             />
+          ),
         },
         {
-            key: 'description',
-            title: 'Popis',
-            input: (form) => <input
-                type="text"
-                id="description"
-                {...form.register('value.description', { required: 'Description is required' })}
-                className="w-full p-2 border rounded"
+          key: 'description',
+          title: 'Popis',
+          input: (form) => (
+            <input
+              type="text"
+              id="description"
+              {...form.register('value.description', {
+                required: 'Description is required',
+              })}
+              className="w-full p-2 border rounded"
             />
+          ),
         },
         {
-            key: 'url',
-            title: 'Profilový obrázek',
-            input: (form) =>
+          key: 'url',
+          title: 'Profilový obrázek',
+          input: (form) => (
             <div>
-                <input
+              <input
                 type="file"
                 id="url"
-                {...form.register('value.url', speaker ? {} : { required: 'Image is required' })}
+                {...form.register(
+                  'value.url',
+                  speaker ? {} : { required: 'Image is required' },
+                )}
                 onChange={handleFileChange}
                 className="w-full p-2 border rounded"
-            />
+              />
             </div>
-                
-        }
-    ]} 
-    onSubmit={(speaker) => {
-        onSubmit(speaker, image!!)
-    }} 
-    close={close} 
-    isOpen={isOpen} 
+          ),
+        },
+      ]}
+      onSubmit={(speaker) => {
+        onSubmit(speaker, image!!);
+      }}
+      close={close}
+      isOpen={isOpen}
     />
-    );
-}
+  );
+};
