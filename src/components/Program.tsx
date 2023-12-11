ea0dd96639ff14a53e6cc9145d3875cf5ca567c6
Marek Seďa <marek.seda@gmail.com>
2023-12-11 18:43:01 +0100

    fix program

--- src/components/Program.tsx
+++ src/components/Program.tsx
@@ -1,22 +1,26 @@
 'use client';
 
 import { Timeline } from '@/components/Timeline';
-import { useEvents } from '@/app/api/api';
+import {useEvents, useSpeakers} from '@/app/api/api';
 
 export default function Program() {
-  const { data, isLoading } = useEvents();
 
-  if (isLoading) return <h1>Loading...</h1>;
+    const {data: events, isLoading: isLoadingEvents} = useEvents();
+    const {data: speakers, isLoading: isLoadingSpeakers} = useSpeakers();
 
-  return (
-    <>
-      <h1 className="bg-red font-bold p-10 text-3xl text-center">
-        Ahoj Marek :)
-      </h1>
-      <h1 className="bg-red pb-10 text-3xl text-center">
-        Do začiatku akcie ostáva 2 dni 14 hodín 15 minút
-      </h1>
-      <Timeline events={data} />
-    </>
-  );
+    if (isLoadingEvents || isLoadingSpeakers) return (<h1>Loading...</h1>)
+
+    if (!speakers || !events) return (<h1>Unable to fetch data...</h1>)
+
+    return (
+        <>
+            <h1 className="bg-red font-bold p-10 text-3xl text-center">
+                Ahoj Marek :)
+            </h1>
+            <h1 className="bg-red pb-10 text-3xl text-center">
+                Do začiatku akcie ostáva 2 dni 14 hodín 15 minút
+            </h1>
+            <Timeline events={events} speakers={speakers} />
+        </>
+    );
 }
